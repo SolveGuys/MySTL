8bf5b8a82a6489e3e19229be7c996ccecd469472
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-09-19 16:18:20 +0900

    수정중

--- MySTL/src/iterator/iterator.h
+++ MySTL/src/iterator/iterator.h
@@ -18,6 +18,14 @@ namespace my
 		using pointer = Pointer;
 		using reference = Reference;
 		
+		//포인터를 위한 iterator_traits
+		// random_access_iterator_tag iterator_category;
+		// _Ty value_type;
+		// ptrdiff_t difference_type;
+		// ptrdiff_t distance_type;    
+		// _Ty* pointer;
+		// _Ty& reference;
+		
 		base_iterator() 
 			:ptr(nullptr)
 		{} // default is nullptr
@@ -36,3 +44,32 @@ namespace my
 	struct bidirectional_iterator_tag : public forward_iterator_tag { };
 	struct random_access_iterator_tag : public bidirectional_iterator_tag { };
 }
+
+/* 이후 
+//input_iterator_tag ??
+template< class IteratorType >
+void Advance( IteratorType _iterator, int _offset, input_iterator_tag )
+{
+	while( --_offset )
+		++_iterator;
+}
+//random_access_iterator_tag
+template< class IteratorType >
+void Advance( IteratorType _iterator, int _offset, random_access_iterator_tag )
+{
+	_iterator += _offset;
+}
+//bidirectional_iterator_tag
+template< class IteratorType >
+void Advance( IteratorType _iterator, int _offset, bidirectional_iterator_tag)
+{
+	if(_offset>=0){
+	while
+}
+template< class IteratorType >
+void Advance( IteratorType _iterator, int _offset )
+{
+	Advance( _iterator, _offset, typename IteratorType::iterator_category() );
+}
+
+*/
